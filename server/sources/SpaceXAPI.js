e8037c9b3b37d8d7ca442e8ad5b092421b339515
laynef <laynefaler@gmail.com>
2020-11-07 11:07:37 -0800

    Added rocket queries

--- server/sources/SpaceXAPI.js
+++ server/sources/SpaceXAPI.js
@@ -3,7 +3,7 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 class SpaceXAPI extends RESTDataSource {
     constructor() {
         super();
-        this.baseURL = 'https://api.spacexdata.com/v3/'
+        this.baseURL = 'https://api.spacexdata.com/v4/'
     }
 
     async getAllRockets() {
--- server/utils/index.js
+++ server/utils/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const getTypeDef = (filePath) => 
-    fs.readFileSync(path.join(__dirname, '..', 'typeDefs', filePath));
+    fs.readFileSync(path.join(__dirname, '..', 'typeDefs', 'graphql', filePath));
 
 module.exports = {
     getTypeDef,
