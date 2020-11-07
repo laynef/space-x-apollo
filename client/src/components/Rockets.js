ee244bb775fe917be9499f3a33bfebbf372488a7
laynef <laynefaler@gmail.com>
2020-11-07 11:32:32 -0800

    Added rockets

--- client/src/components/Rockets.js
+++ client/src/components/Rockets.js
@@ -7,14 +7,30 @@ import logo from "../images/logo.svg";
 function Rockets() {
     const gql = loader('../graphql/rockets.graphql');
     const { loading, error, data } = useQuery(gql);
+    console.log(data)
 
     return (
         <div className="App">
             <div className="App-header">
+                {data && (
+                    <div className="w-100 d-flex flex-column align-items-center">
+                        {data.rockets.map((rocket, key) => (
+                            <div key={key}>
+                                
+                            </div>
+                        ))}
+                    </div>
+                )}
                 {loading && (
                     <>
                         <img className="App-logo" src={logo} alt="" />
                         <p>Loading...</p>
+                    </>
+                )}
+                {error && (
+                    <>
+                        <img className="App-logo" src={logo} alt="" />
+                        <p>Internal Server Error</p>
                     </>
                 )}
             </div>
